6531d82f901ae8a06baa1f8b94a841cdfa79ba5e
dipeshay <babasarkar260@gmail.com>
2024-10-26 10:03:35 +0545

    add github code to footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,11 +1,11 @@
-
+import {Link} from "react-router-dom"
 const Footer = () => {
   return (
     <div className='py-3 bg-black  relative text-white'>
         <div className=" bg-black  text-white absolute   w-full  py-10 z-50  md:py-10  ">
          <div className=' container flex gap-3 items-center'>
             <h1 className='text-md text-gray-400'>Built by <span className='underline font-medium'> Dipesh </span> </h1>
-            <span className='flex text-sm text-gray-400'>The source code is available on  <span className='hover:underline ml-1'> Github </span></span>
+         <Link to="https://github.com/dipeshxhy/netflix-clone">  <span className='flex text-sm text-gray-400'>The source code is available on  <span className='hover:underline ml-1'> Github </span></span></Link> 
          </div>
         </div>
     </div>
